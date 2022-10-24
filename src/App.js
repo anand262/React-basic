03b6f7336db30bde432e025cc2e1d8c112c70d18
Anand_Shinde <98341248+anand262@users.noreply.github.com>
2022-10-24 23:30:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import LifeCycleA from "./Commponents/LifeCycleA";
+
+function App() {
+  return (
+    <div className="App">
+      <LifeCycleA />
+    </div>
+  );
+}
+
+export default App;
